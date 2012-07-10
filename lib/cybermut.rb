a95cfe2c45ccfd9c1e09b490ab6caf87aa62b1af
Christian Gillot <cgillot@immateriel.fr>
2012-07-10 15:50:56 +0200

    fix r19

--- lib/cybermut.rb
+++ lib/cybermut.rb
@@ -5,7 +5,9 @@ class String
       unless other.kind_of? String
     raise ArgumentError, "Can't bitwise-XOR strings of different length" \
       unless self.length == other.length
-    result = (0..self.length-1).collect { |i| self[i] ^ other[i] }
+    my_bytes = self.bytes.to_a
+    other_bytes = other.bytes.to_a
+    result = (0..self.length-1).collect { |i| my_bytes[i] ^ other_bytes[i] }
     result.pack("C*")
   end
 end
